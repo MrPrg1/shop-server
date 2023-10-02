8f4ec19a251a0ecc1d013a0db43d59d2dd6d4964
pouya <MrPrg1@gmail.com>
2023-10-02 09:19:29 -0700

    created Models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Brand, Product, Coupon, ProductOrder, Address, Order, Comment, Favorit
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Coupon)
+admin.site.register(ProductOrder)
+admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(Comment)
+admin.site.register(Favorit)
--- store/models.py
+++ store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from core.models import BaseModel
+from user.models import BaseUser
+
 
 class Category(BaseModel):
     name = models.CharField(max_length=200)
@@ -37,3 +39,51 @@ class Product(BaseModel):
         return self.name
     
     
+class Coupon(BaseModel):
+    code = models.CharField(max_length=10, unique=True)
+    discount = models.DecimalField(decimal_places=2, max_digits=20)
+    discount_type = models.CharField(max_length=40)
+    active = models.BooleanField(default=True)
+
+
+class ProductOrder(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField(default=1)
+
+
+
+
+class Address(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name='user_address')
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+
+
+
+class Order(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    product = models.ManyToManyField(ProductOrder)
+    status = models.CharField(max_length=100, default='pending')
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+
+
+class Comment(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    comment = models.TextField()
+    to = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
+
+
+
+class Favorit(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'product')
+
